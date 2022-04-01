a64c6f422fa3ee7edae8176212a9082b2e4f0153
Torres <gustavo.storres@ufrpe.br>
2022-04-01 13:42:57 -0300

    Update associação Paciente-Consulta, Medico-Consulta

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -2,6 +2,6 @@ class Paciente < ApplicationRecord
   has_one :endereco, dependent: :destroy
   accepts_nested_attributes_for :endereco, allow_destroy: true
 
-  has_many :consultums
+  has_many :consultums, dependent: :destroy
   has_many :medicos, through: :consultums
 end
