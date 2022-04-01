adba980266b4f90cee9e4c66f74acde6d75277f0
Torres <gustavo.storres@ufrpe.br>
2022-04-01 10:23:19 -0300

    Scaffold Consulta

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_01_131344) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_01_131936) do
+  create_table "consulta", force: :cascade do |t|
+    t.date "data"
+    t.string "horario"
+    t.integer "paciente_id", null: false
+    t.integer "medico_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["medico_id"], name: "index_consulta_on_medico_id"
+    t.index ["paciente_id"], name: "index_consulta_on_paciente_id"
+  end
+
   create_table "enderecos", force: :cascade do |t|
     t.string "cep"
     t.string "cidade"
@@ -42,5 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_01_131344) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "consulta", "medicos"
+  add_foreign_key "consulta", "pacientes"
   add_foreign_key "enderecos", "pacientes"
 end
