911032db3fe4909b37c336fbe6236f7158b91a3d
Torres <gustavo.storres@ufrpe.br>
2022-04-02 12:50:11 -0300

    Validates Endereco

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,3 +1,14 @@
 class Endereco < ApplicationRecord
   belongs_to :paciente
+
+  validates_format_of :cep, with: /\A\d{5}-\d{3}\z/, presence: true,
+                      message: 'formato: XXXXX-XXX'
+  validates :cidade, presence: true, length: {in: 3..30}, format: {with: /\A[a-zA-Z]+\z/,
+                                                                   message: "Use apenas letras"}
+  validates :bairro, presence: true, length: {in: 3..30}, format: {with: /\A[a-zA-Z]+\z/,
+                                                                   message: "Use apenas letras"}
+  validates :logradouro, presence: true, length: {in: 3..30}, format: {with: /\A[a-zA-Z]+\z/,
+                                                                       message: "Use apenas letras"}
+  validates :complemento, presence: true, length: {in: 1..10}, format: {with:/\A[a-zA-Z0-9\s]+\z/i,
+                                                                        message: "Use apenas letras e números"}
 end
