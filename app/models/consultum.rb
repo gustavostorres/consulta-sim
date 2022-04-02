7e6a294e9aca085c23655771fb08276b2c2faa27
Torres <gustavo.storres@ufrpe.br>
2022-04-02 17:03:47 -0300

    Correção validate consulta

--- app/models/consultum.rb
+++ app/models/consultum.rb
@@ -2,8 +2,8 @@ class Consultum < ApplicationRecord
   belongs_to :paciente
   belongs_to :medico
 
-  validate consulta_valida
-  
+  validate :consulta_valida
+
   def consulta_valida
     if data.present? && data < Date.today
       errors.add(:data, "invalida, não se pode agendar uma consulta nesse dia")
