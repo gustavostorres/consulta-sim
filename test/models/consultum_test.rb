ef62ba2ee7a7e7d041043676a1697f73e892bc2b
Torres <gustavo.storres@ufrpe.br>
2022-04-13 13:17:13 -0300

    Update test consulta

--- test/models/consultum_test.rb
+++ test/models/consultum_test.rb
@@ -17,8 +17,14 @@ class ConsultumTest < ActiveSupport::TestCase
     assert consulta.save
   end
 
-  test 'Criando consulta com dados invalidos' do
-    consulta = Consultum.new data: DateTime.now, horario: 'asdw'
+  test 'Criando consulta com dados de medico invalido' do
+    paciente = Paciente.new nome: 'Gustavo Torres', dataNasc: DateTime.now,
+                            cpf: '027.760.354-46', email: 'dika_ka@hotmail.com'
+    assert paciente.save
+    medico = Medico.new nome: 'Gustavo Torres', cpf: '027.760.354-46', email: 'dika_ka@hotmail.com',
+                        especialidade: 'Ortopedista', crm: '12345678-9'
+    assert medico.save
+    consulta = Consultum.new data: DateTime.now, horario: '10:00', paciente_id: paciente.id, medico_id: medico.nome
     assert_not consulta.save
   end
 
@@ -31,7 +37,6 @@ class ConsultumTest < ActiveSupport::TestCase
     assert medico.save
     consulta = Consultum.new data: DateTime.now, horario: '10:00', paciente_id: paciente.nome, medico_id: medico.id
     assert_not consulta.save
-
   end
   # test "the truth" do
   #   assert true
