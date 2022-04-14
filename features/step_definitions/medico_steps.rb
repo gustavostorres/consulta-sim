924c0b042b4e7f6d0f28f296fb541f384f07be11
Torres <gustavo.storres@ufrpe.br>
2022-04-13 22:56:18 -0300

    Atualizando nome das variaveis step medico

--- features/step_definitions/medico_steps.rb
+++ features/step_definitions/medico_steps.rb
@@ -3,17 +3,17 @@ Given('Estou na pagina de cadastrar medico') do
   expect(page).to have_current_path('/medicos/new')
 end
 
-When('eu preencho os dados nome {string} cpf {string} email {string} especialidade {string} crm {string} e clico cadastrar') do |string, string2, string3, string4, string5|
-  fill_in 'medico[nome]', :with => string
-  fill_in 'medico[cpf]', :with => string2
-  fill_in 'medico[email]', :with => string3
-  fill_in 'medico[especialidade]', :with => string4
-  fill_in 'medico[crm]', :with => string5
+When('eu preencho os dados nome {string} cpf {string} email {string} especialidade {string} crm {string} e clico cadastrar') do |nome, cpf, email, especialidade, crm|
+  fill_in 'medico[nome]', :with => nome
+  fill_in 'medico[cpf]', :with => cpf
+  fill_in 'medico[email]', :with => email
+  fill_in 'medico[especialidade]', :with => especialidade
+  fill_in 'medico[crm]', :with => crm
   click_button 'Create Medico'
 end
 
-Then('vejo que o meu cadastro {string} foi realizado') do |string|
-  expect(page).to have_content(string)
+Then('vejo que o meu cadastro {string} foi realizado') do |crm|
+  expect(page).to have_content(crm)
   expect(page).to have_current_path(medicos_path + '/' +
                                       Medico.last.id.to_s)
 end
@@ -24,24 +24,24 @@ Given('Estou na pagina de medico') do
   expect(page).to have_current_path('/medicos')
 end
 
-Given('o medico de nome {string} cpf {string} email {string} especialidade {string} crm {string} existe') do |string, string2, string3, string4, string5|
+Given('o medico de nome {string} cpf {string} email {string} especialidade {string} crm {string} existe') do |nome, cpf, email, especialidade, crm|
   visit 'medicos/new'
-  fill_in 'medico[nome]', :with => string
-  fill_in 'medico[cpf]', :with => string2
-  fill_in 'medico[email]', :with => string3
-  fill_in 'medico[especialidade]', :with => string4
-  fill_in 'medico[crm]', :with => string5
+  fill_in 'medico[nome]', :with => nome
+  fill_in 'medico[cpf]', :with => cpf
+  fill_in 'medico[email]', :with => email
+  fill_in 'medico[especialidade]', :with => especialidade
+  fill_in 'medico[crm]', :with => crm
   click_button 'Create Medico'
-  expect(page).to have_content(string5)
+  expect(page).to have_content(crm)
 end
 
-When('eu clico para remover o medico de crm {string}') do |string|
-  expect(page).to have_content(string)
+When('eu clico para remover o medico de crm {string}') do |crm|
+  expect(page).to have_content(crm)
   click_button "Destroy this medico"
 end
 
-Then('eu vejo que o medico com crm {string} foi corretamente removido') do |string|
-  expect(page).to have_no_content(string)
+Then('eu vejo que o medico com crm {string} foi corretamente removido') do |crm|
+  expect(page).to have_no_content(crm)
   expect(page).to have_current_path('/medicos')
 end
 
